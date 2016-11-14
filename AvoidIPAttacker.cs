a3aa774ebc5cc0a01d06c56abaeb824b8e4403d7
kmonkey9006 <1714404171@qq.com>
2016-11-14 14:57:42 +0800

    c#特性https://github.com/kmonkey9006/SecurityGuard.git 案例之防网络攻击

---
+++ AvoidIPAttacker.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityGuard
+{
+    public interface IAttack
+    {
+        string key1 { get; set; }
+        string key2 { get; set; }
+        string key3 { get; set; }
+        DateTime attackedtime { get; set; }
+        DateTime lastime { get; set; }
+        int times { get; set; }
+        bool attacking();
+    }
+    public interface IAvoidAttacker
+    {
+        bool Attacking(string key1, string key2, string key3);
+
+        int MaxAttackingTimes { get; set; }
+        int MinPendingMinutes { get; set; }
+        int AttackedIntervalTimes { get; set; }
+        void Init(string configkey);
+        void Init(IAvoidAttacker attacker);
+        void Init();
+        string Tip();
+    }
+    public class Attack : IAttack
+    {
+        public Attack() { }
+        public Attack(string key1, string key2, string key3)
+        {
+            this.key1 = key1;
+            this.key2 = key2;
+            this.key3 = key3;
+            attackedtime = lastime = DateTime.Now;
+            times = 1;
+        }
+        public string key1 { get; set; }
+
+        public string key2 { get; set; }
+
+        public string key3 { get; set; }
+
+        public int times { get; set; }
+
+        public DateTime attackedtime { get; set; }
+        public DateTime lastime { get; set; }
+
+        public bool attacking()
+        {
+            if (lastime.AddMinutes(AvoidIPAttacker.Instance.MinPendingMinutes) < DateTime.Now)
+            {
+                times = 1;
+                lastime = attackedtime = DateTime.Now;
+                return false;
+            }
+            if (times < AvoidIPAttacker.Instance.MaxAttackingTimes)
+            {
+                times++;
+                lastime = DateTime.Now;
+                return false;
+            }
+            times++;
+            lastime = DateTime.Now;
+            return true;
+        }
+
+
+    }
+    public class AvoidIPAttacker : IAvoidAttacker
+    {
+        public AvoidIPAttacker()
+        {
+            AttackedIntervalTimes = 10;
+            MaxAttackingTimes = 10;
+            MinPendingMinutes = 60;
+            _lastrefreshime = DateTime.Now;
+        }
+        public string Tip()
+        {
+            string msg = "";
+            if (MinPendingMinutes < 60)
+                msg = string.Format("您尝试的登录次数超过{0}次,请{1}分钟后再试", MaxAttackingTimes, MinPendingMinutes);
+            else if (MinPendingMinutes % 60 == 0)
+                msg = string.Format("您尝试的登录次数超过{0}次,请{1}小时后再试", MaxAttackingTimes, MinPendingMinutes / 60);
+            else
+                msg = string.Format("您尝试的登录次数超过{0}次,请{1}小时{2}分钟后再试", MaxAttackingTimes, MinPendingMinutes / 60, MinPendingMinutes % 60);
+            return msg;
+
+        }
+        static IAvoidAttacker _instance = new AvoidIPAttacker();
+        public static IAvoidAttacker Instance { get { return _instance; } }
+
+        public static List<IAttack> _attacks = new List<IAttack>();
+
+        public bool Attacking(string key1, string key2 = null, string key3 = null)
+        {
+            lock (_locker1)
+            {
+                Init();
+                Refresh();
+                if (string.IsNullOrWhiteSpace(key1)) return false;
+                if (string.IsNullOrWhiteSpace(key2) && string.IsNullOrWhiteSpace(key3)) return false;
+                IAttack attack = null;
+                if (key3 == null && key2 == null)
+                    attack = _attacks.SingleOrDefault(a => a.key1 == key1);
+                else if (key3 == null)
+                    attack = _attacks.SingleOrDefault(a => a.key1 == key1 && a.key2 == key2);
+                else attack = _attacks.SingleOrDefault(a => a.key1 == key1 && a.key2 == key2 && a.key3 == key3);
+                if (attack == null)
+                {
+                    _attacks.Add(new Attack(key1, key2, key3));
+                    return false;
+                }
+                return attack.attacking();
+            }
+        }
+
+        public DateTime _lastrefreshime { get; set; }
+        static object _locker = new object();
+        static object _locker1 = new object();
+        public void Refresh()
+        {
+            lock (_locker)
+            {
+                if (_lastrefreshime.AddMinutes(MinPendingMinutes) > DateTime.Now)
+                {
+                    _attacks.RemoveAll(a => a.lastime.AddMinutes(MinPendingMinutes) < DateTime.Now);
+                    _lastrefreshime = DateTime.Now;
+                }
+            }
+        }
+
+        public int MaxAttackingTimes { get; set; }
+
+        public int MinPendingMinutes { get; set; }
+
+        public int AttackedIntervalTimes { get; set; }
+
+
+        public void Init(string configkey = "AvoidAttackingConfig")
+        {
+            string mt = System.Configuration.ConfigurationManager.AppSettings.Get(configkey);
+            if (string.IsNullOrWhiteSpace(mt))
+                return;
+
+            var keys = mt.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+            if (keys == null || keys.Length != 3) return;
+
+            int v = 0;
+            if (int.TryParse(keys[0], out v))
+            {
+                AttackedIntervalTimes = v;
+            }
+            v = 0;
+            if (int.TryParse(keys[1], out v))
+            {
+                MaxAttackingTimes = v;
+            }
+            v = 0;
+            if (int.TryParse(keys[2], out v))
+            {
+                MinPendingMinutes = v;
+            }
+        }
+
+        public void Init()
+        {
+
+            string mt = System.Configuration.ConfigurationManager.AppSettings.Get("AttackingAvoidMaxPendingTimes");
+            if (string.IsNullOrWhiteSpace(mt))
+                return;
+
+            var keys = mt.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+            if (keys == null || keys.Length != 3) return;
+
+            int v = 0;
+            if (int.TryParse(keys[0], out v))
+            {
+                AttackedIntervalTimes = v;
+            }
+            v = 0;
+            if (int.TryParse(keys[1], out v))
+            {
+                MaxAttackingTimes = v;
+            }
+            v = 0;
+            if (int.TryParse(keys[2], out v))
+            {
+                MinPendingMinutes = v;
+            }
+        }
+
+
+        public void Init(IAvoidAttacker attacker)
+        {
+            _instance = attacker;
+        }
+    }
+}
---
+++ NeedSecurityGuardAttribute.cs
@@ -0,0 +1,262 @@
+﻿using Common.Logger;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SecurityGuard
+{
+    public enum SecurityGuard
+    {
+        None = 0,
+        LogTrace = 1,
+        HotLinking = 2,
+        Attacking = 4,
+        NewSession = 8,
+        AjaxRequest = 16,
+        Uploading = 32,
+        All = LogTrace | HotLinking | Attacking | NewSession | AjaxRequest | Uploading
+    }
+
+    public class NeedSecurityGuardAttribute : ActionFilterAttribute
+    {
+        static NeedSecurityGuardAttribute()
+        {
+            _avoidattacker.Init("IPAttackingAvoidMaxPendingTimes");
+        }
+
+        SecurityGuard _sg = SecurityGuard.None;
+        string _extnames = "", _denyextnames = ".exe|.bat|msi|.vbs|.js|.cmd|.src|.reg|.com|.lnk|.pif|.dll|.drv|.fon|.jar|.css|.asp|.aspx|.jsp|.html|.stm|.axd|.vbhtml|.cshtml|.asmx|.aspq|.cshtm|.vbhtm|.rem|.soap|.cer|.ashx|.shtm|.sthml";
+
+        public SecurityGuard SG { get { return _sg; } }
+        public NeedSecurityGuardAttribute(SecurityGuard sg = SecurityGuard.None, string extnames = ".ico|.jpg|.gif|.png|.doc|.docx|.xls|.xlsx|.ppt|.ppts|.pps|.txt|.zip|.rar")
+           
+        {
+            _sg = sg;
+            _extnames = extnames;
+        }
+        static List<string> allowedextnames = new List<string>();
+        static List<string> denyextnames = new List<string>();
+        static AvoidIPAttacker _avoidattacker = new AvoidIPAttacker();
+        public static List<string> AllowedAuthorities = new List<string>();
+        static bool hotlinker_inited = false;
+        static bool upload_inited = false;
+        ILogger logger = new Logger4Net("JobServer");
+        public bool IsHotLinking(string authority)
+        {
+            if (!hotlinker_inited)
+            {
+                if (AllowedAuthorities.Count == 0)
+                {
+                    var ads = ConfigurationManager.AppSettings["SecurityGuard_AllowedDomain"];
+                    if (!string.IsNullOrWhiteSpace(ads))
+                    {
+                        AllowedAuthorities.AddRange(ads.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries));
+                    }
+                    AllowedAuthorities.Add("localhost");
+                }
+                hotlinker_inited = true;
+            }
+            return !AllowedAuthorities.Contains(authority);
+        }
+
+
+        bool IsHotLinking(HttpRequestBase request)
+        {
+            if (!IsHotLinking(request.Url.DnsSafeHost)) return false;
+            return request.UrlReferrer == null;
+            //return IsHotLinking(request.Url.DnsSafeHost);
+        }
+        bool IsNewSession(HttpContextBase request)
+        {
+            return request.Session.IsNewSession;
+        }
+        bool IsAjaxRequest(HttpRequestBase request)
+        {
+            return request.IsAjaxRequest();
+        }
+
+        bool IsAttacking(HttpRequestBase request)
+        {
+            return _avoidattacker.Attacking(request.GetIP());
+        }
+        bool DenyUploading(HttpRequestBase request)
+        {
+            if (!upload_inited)
+            {
+                denyextnames.AddRange(_denyextnames.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries));
+            }
+            allowedextnames.AddRange(_extnames.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries));
+            bool deny = true;
+            foreach (var key in request.Files.AllKeys)
+            {
+                if (denyextnames.Count > 0)
+                {
+                    if (denyextnames.Any(n => request.Files[key].FileName.EndsWith(n, StringComparison.OrdinalIgnoreCase)))
+                    {
+                        deny = true;
+                        break;
+                    }
+                }
+                if (allowedextnames.Count > 0)
+                {
+                    if (allowedextnames.Any(n => request.Files[key].FileName.EndsWith(n, StringComparison.OrdinalIgnoreCase)))
+                    {
+                        deny = false;
+                        continue;
+                    }
+                }
+            }
+            return deny;
+        }
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            //base.OnActionExecuting(filterContext);
+            //return;
+            if (filterContext.IsChildAction) return;
+            bool refuse = false;
+            string msg = null;
+            if ((SG & SecurityGuard.HotLinking) == SecurityGuard.HotLinking)
+            {
+                refuse = refuse || IsHotLinking(filterContext.HttpContext.Request);
+                if (refuse)
+                    msg = " 盗链可耻";
+            }
+            if ((SG & SecurityGuard.NewSession) == SecurityGuard.NewSession)
+            {
+                refuse = refuse || IsNewSession(filterContext.HttpContext);
+                if (refuse)
+                    msg = " 被中断";
+            }
+            if ((SG & SecurityGuard.AjaxRequest) == SecurityGuard.AjaxRequest)
+            {
+                refuse = refuse || IsAjaxRequest(filterContext.HttpContext.Request);
+                if (refuse)
+                    msg = " 被禁止";
+            }
+            if ((SG & SecurityGuard.Attacking) == SecurityGuard.Attacking)
+            {
+                refuse = refuse || IsAttacking(filterContext.HttpContext.Request);
+                if (refuse)
+                    msg = " 被拒绝";
+            }
+            if ((SG & SecurityGuard.Uploading) == SecurityGuard.Uploading)
+            {
+                refuse = refuse || DenyUploading(filterContext.HttpContext.Request);
+                if (refuse)
+                    msg = " 禁止上传";
+            }
+            if ((SG & SecurityGuard.LogTrace) == SecurityGuard.LogTrace || refuse)
+                logger.Trace("URL:" + filterContext.HttpContext.Request.Url.OriginalString + msg + filterContext.HttpContext.Info());
+            if (!refuse) return;
+
+            //filterContext.HttpContext.Response.Redirect("www.google.com");
+            filterContext.HttpContext.Response.Write(msg);
+            filterContext.HttpContext.Response.End();
+            filterContext.HttpContext.Response.Close();
+            //filterContext.HttpContext.Session.Timeout = 0;
+            filterContext.HttpContext.Session.Abandon();
+            var result = new ContentResult() { Content = msg };
+            result.Content = msg;
+            filterContext.Result = result;
+            //filterContext.HttpContext.Response.Redirect("/");
+        }
+
+    }
+
+    internal static class ext
+    {
+
+        public static string Info(this HttpContextBase context)
+        {
+            StringBuilder sb = new StringBuilder();
+            HttpBrowserCapabilitiesBase bc = context.Request.Browser;
+            sb.AppendLine();
+            sb.AppendLine("=====Browser Capabilities:=====");
+            sb.AppendLine("Type = " + bc.Type);
+            sb.AppendLine("Version = " + bc.Version);
+            sb.AppendLine("MobileDeviceManufacturer = " + bc.MobileDeviceManufacturer);
+            sb.AppendLine("MobileDeviceModel = " + bc.MobileDeviceModel);
+            sb.AppendLine("=====LogonUserIdentity:=====");
+            sb.AppendLine("IsAuthenticated: " + context.Request.LogonUserIdentity.IsAuthenticated);
+            sb.AppendLine("User: " + context.Request.LogonUserIdentity.User);
+            sb.AppendLine("Name: " + context.Request.LogonUserIdentity.Name);
+            sb.AppendLine("Token: " + context.Request.LogonUserIdentity.Token);
+            sb.AppendLine("=====User:=====");
+            sb.AppendLine("IP: " + context.Request.GetIP());
+            sb.AppendLine("UserAgent: " + context.Request.UserAgent);
+            sb.AppendLine("UserHostAddress: " + context.Request.UserHostAddress);
+            sb.AppendLine("UserHostName: " + context.Request.UserHostName);
+
+            sb.AppendLine("=====Params:=====");
+            sb.AppendLine("LOCAL_ADDR: " + context.Request.Params["LOCAL_ADDR"]);
+            sb.AppendLine("REMOTE_ADDR: " + context.Request.Params["REMOTE_ADDR"]);
+            sb.AppendLine("REMOTE_HOST: " + context.Request.Params["REMOTE_HOST"]);
+            sb.AppendLine("REMOTE_PORT: " + context.Request.Params["REMOTE_PORT"]);
+            sb.AppendLine("REQUEST_METHOD: " + context.Request.HttpMethod);
+            sb.AppendLine("IsAjaxRequest: " + context.Request.IsAjaxRequest());
+
+
+            // Write user info to log
+            sb.AppendLine("=====Form:=====");
+            foreach (var key in context.Request.Form.AllKeys)
+            {
+                sb.AppendLine(key + ": " + context.Request.Params[key]);
+            }
+            sb.AppendLine("=====QueryString:=====");
+            foreach (var key in context.Request.QueryString.AllKeys)
+            {
+                sb.AppendLine(key + ": " + context.Request.Params[key]);
+            }
+            sb.AppendLine("=====Files:=====");
+            foreach (var key in context.Request.Files.AllKeys)
+            {
+                sb.AppendFormat("{0},{1},{2}", key, context.Request.Files[key].FileName, context.Request.Files[key].ContentLength);
+            }
+            // sb.AppendLine();
+            // Write user info to log
+            //sb.AppendLine("=====Headers:=====");
+            //foreach (var key in context.Request.Headers.AllKeys)
+            //{
+            //    sb.AppendLine(key + ": " + context.Request.Headers[key]);
+            //}
+            return sb.ToString();
+        }
+
+        internal static string GetIP(this HttpRequestBase request)
+        {
+            #region getIp
+            // 穿过代理服务器取远程用户真实IP地址
+            string Ip = string.Empty;
+            if (request.ServerVariables["HTTP_VIA"] != null)
+            {
+                if (request.ServerVariables["HTTP_X_FORWARDED_FOR"] == null)
+                {
+                    if (request.ServerVariables["HTTP_CLIENT_IP"] != null)
+                        Ip = request.ServerVariables["HTTP_CLIENT_IP"].ToString();
+                    else
+                        if (request.ServerVariables["REMOTE_ADDR"] != null)
+                        Ip = request.ServerVariables["REMOTE_ADDR"].ToString();
+                    else
+                        Ip = "127.0.0.1";
+                }
+                else
+                    Ip = request.ServerVariables["HTTP_X_FORWARDED_FOR"].ToString();
+            }
+            else if (request.ServerVariables["REMOTE_ADDR"] != null)
+            {
+                Ip = request.ServerVariables["REMOTE_ADDR"].ToString();
+            }
+            else
+            {
+                Ip = "127.0.0.1";
+            }
+            return Ip;
+            #endregion
+        }
+    }
+}
